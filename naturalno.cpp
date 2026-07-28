a2d407ad380f17793b9428577b5de94d7cd21f8b
lovely joshi <lovelyjoshi7002@gmail.com>
2026-07-28 18:57:34 +0530

    added new changes

---
+++ naturalno.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,sum=0;
+    for(i=0;i<10;i++)
+    {
+        sum=sum +i;
+    }
+    cout<<"The sum of first 10 natural numbers are ="<<sum;
+    return 0;
+}
