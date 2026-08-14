39386f0b7155321fd42aad2614561003ea164cd8
lovely joshi <lovelyjoshi7002@gmail.com>
2026-08-14 14:52:57 +0530

    perform addition

---
+++ sum1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+   // Declare variables
+int num1, num2, sum;
+// Take input from the user
+cout << "Enter two integers: ";
+ cin >> num1 >> num2;
+sum = num1 + num2;
+// Display the result
+cout << "Sum = " << sum << endl;
+return 0; 
+}
