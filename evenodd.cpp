7fb4483dcc6a7d8e8c3cbbf48a04cb59d704a440
lovely joshi <lovelyjoshi7002@gmail.com>
2026-08-16 07:54:42 +0530

    even odd

---
+++ evenodd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter number"<<endl;
+    cin>>num;
+    if(num%2==0)
+    {
+        cout<<"even"<<endl;
+    }
+    else
+    {
+        cout<<"odd"<<endl;
+    }
+    return 0;
+}
