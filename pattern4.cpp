6a69c7b7eb3906f7fd27a0b667523d2bc487c08e
lovely joshi <lovelyjoshi7002@gmail.com>
2026-08-27 21:17:27 +0530

    pattern print

---
+++ pattern4.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+void pattern4(int n){
+    for(int i=0; i<=n; i++)
+    {
+        //space
+        for(int j=0; j<=n-i-1;j++){
+            cout<<" ";
+        }
+        //star
+        for(int j=0; j<=2*i+1;j++){
+            cout<<"*";
+        }
+        //space
+        for(int j=0; j<=n-i-1;j++){
+            cout<<" ";
+        }
+    cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"enter number";
+    cin>>n;
+    pattern4(n);
+    return 0;
+}
